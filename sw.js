e567cd20fd50e5ee68d61dcaef265ef81c2d960a
LeticiaC16 <leticia.caseiro@aguasdesaofrancisco.com.br>
2025-09-02 16:53:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'fds-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/FDS_ORTONEX_023.pdf',
+  '/FDS_HIPOCLORITO_65.pdf',
+  '/FDS_TRICLORO.pdf'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
